49c55e3a06302a221788afd10f25c4e0704680e8
hosein-fanai <hosein.fanai@gmail.com>
2024-02-29 16:01:25 +0330

    Trained the final model with 100k dataset containing a larger scale of font size for numbers.

--- history_plotting.py
+++ history_plotting.py
@@ -16,8 +16,8 @@ def plot_history(history):
     train_bboxes_mae = history["bboxes_mae"]
     val_bboxes_mae = history["val_bboxes_mae"]
 
-    train_card_type_acc = history["card_type_accuracy"]
-    val_card_type_acc = history["val_card_type_accuracy"]
+    train_card_type_acc = history.get("card_type_accuracy", [0]*len(history["loss"]))
+    val_card_type_acc = history.get("val_card_type_accuracy", [0]*len(history["loss"]))
 
     plt.figure(figsize=(15, 15))
 
@@ -26,32 +26,36 @@ def plot_history(history):
     plt.plot(epochs, val_loss, label="Validation")
     plt.xlabel("Epoch")
     plt.ylabel("Total Loss")
+    plt.legend()
 
     plt.subplot(3, 2, 2)
     plt.plot(epochs, train_card_type_acc, label="Train")
     plt.plot(epochs, val_card_type_acc, label="Validation")
     plt.xlabel("Epoch")
     plt.ylabel("Card Type Accuracy")
+    plt.legend()
 
     plt.subplot(3, 2, 3)
     plt.plot(epochs, train_confs_acc, label="Train")
     plt.plot(epochs, val_confs_acc, label="Validation")
     plt.xlabel("Epoch")
     plt.ylabel("Confs Accuracy")
+    plt.legend()
 
     plt.subplot(3, 2, 4)
     plt.plot(epochs, train_bboxes_mae, label="Train")
     plt.plot(epochs, val_bboxes_mae, label="Validation")
     plt.xlabel("Epoch")
     plt.ylabel("BBoxes MAE")
+    plt.legend()
 
     plt.subplot(3, 2, 5)
     plt.plot(epochs, train_classes_acc, label="Train")
     plt.plot(epochs, val_classes_acc, label="Validation")
     plt.xlabel("Epoch")
     plt.ylabel("Classes Accuracy")
-
     plt.legend()
+
     plt.show()
 
 
